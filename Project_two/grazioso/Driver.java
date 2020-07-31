f4fba7754f9a863409edfb1bbe516469ad618d7f
Geoff Lange <geoff@reachthemarket.com>
2020-07-31 10:50:45 -0400

    Update Driver.java
    Final commit for grade

--- Project_two/grazioso/Driver.java
+++ Project_two/grazioso/Driver.java
@@ -1,22 +1,63 @@
 
+/* Geoff Lange
+IT-145
+July 20220
+geoffrey.lange@snhu.edu
+ */
+
+import java.util.Scanner;
+
 
 public class Driver {
 
     public static void main(String[] args) {
 
         // Instance variables
+        String userAnimalRequest;
+        String userCountryResidence;
 
 
+
+        // create new scanner for user input
+        Scanner scnr = new Scanner(System.in);
 
         // Create New Dog
+        Dog userDog = new Dog(); // generic constructor for new dog
 
 
 
         // Create New Monkey
+        Monkey userMonkey = new Monkey(); // generic constructor for new dog
 
 
 
         // Method to process request for a rescue animal
+        // TODO: Future Menu option. Currently method not used and could remove the definition but not the statements.
+        public void RequestAnimal(){
+            // Questions for rescue animal request.
+            System.out.println("Please enter a rescue animal type. Either Dog or Monkey");
+            userAnimalRequest = scnr.nextLine();
+            userAnimalRequest = userAnimalRequest.toLowerCase(); // Set to lowercase for validation
+
+            System.out.println("Please enter your country of residence");
+            userCountryResidence = scnr.nextLine();
+            userCountryResidence = userCountryResidence.toLowerCase(); // Set to lowercase for validation
+
+            // Check to see if animal available and reserve if available.
+            if (userCountryResidence.equals(userDog.getInServiceCountry()) && userAnimalRequest.equals("dog")){
+                System.out.println("A dog in your country is available and has been reserved.");
+                userDog.setReserved(true);
+            }
+            else if (userCountryResidence.equals(userMonkey.getInServiceAgency()) && userAnimalRequest.equals("monkey")) {
+                System.out.println("A monkey in your country is available and has been reserved.");
+                userMonkey.setReserved(true);
+            }
+            else {
+                System.out.println("No " + userAnimalRequest + " is available in " + userCountryResidence +".");
+            }
+        }
+    }
+
 
 
 
@@ -24,7 +65,7 @@ public class Driver {
 
 
 
-        // Method to display matrix of aninmals based on location and status/training phase
+        // Method to display matrix of animals based on location and status/training phase
 
 
 
@@ -44,5 +85,6 @@ public class Driver {
 
 
 
-    }
+
+
 }
