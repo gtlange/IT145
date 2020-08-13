b5acb8398463bd735062cbc728ebd4e75e3e08d5
Geoff Lange <geoff@reachthemarket.com>
2020-08-13 18:18:28 -0400

    Update Cruise.java

--- Project_Three/src/Cruise.java
+++ Project_Three/src/Cruise.java
@@ -62,12 +62,49 @@ public class Cruise {
         returnPort = tVar;
     }
 
-    // print cruise details
-    public void printCruiseDetails() {
 
-        // complete this method
+
+    // print cruise details
+    // TODO: Clean this up.
+    public void printCruiseDetails() {
+        int spaceCount;															//Int variables
+        int spaceCount1;
+        int spaceCount2;
+        int spaceCount3;
+        String spaces = "";														//String variables
+        String spaces1 = "";
+        String spaces2 = "";
+        String spaces3 = "";
+        spaceCount = 20 - cruiseName.length();	 								//Determining number of spaces between words.
+        spaceCount1 = 20 - cruiseShipName.length(); 							//Value based on 20 spaces minus length of word received from input
+        spaceCount2 = 20 - departurePort.length(); 								//to keep all output uniform in column format.
+        spaceCount3 = 20 - destination.length();
+
+        for (int i = 1; i <= spaceCount; i++) {									//for loops used to iterate over String spaces and add spaces
+            spaces = spaces + " ";
+        }
+        for (int i = 1; i <= spaceCount1; i++) {
+            spaces1 = spaces1 + " ";
+        }
+        for (int i = 1; i <= spaceCount2; i++) {
+            spaces2 = spaces2 + " ";
+        }
+        for (int i = 1; i <= spaceCount3; i++) {
+            spaces3 = spaces3 + " ";
+        }
+
+
+
+        System.out.println(cruiseName + spaces + cruiseShipName  + spaces1 +	//Print statement using dynamic spaces between variables ensuring
+                departurePort + spaces2 + destination + spaces3 + 				//uniform spacing between words.
+                returnPort);
 
     }
+
+
+
+
+
 
     // method added to print ship's name vice memory address
     @Override
