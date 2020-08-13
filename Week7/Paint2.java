514d958c7e514714ea2ca2fee34cdcc8d989a519
Geoff Lange <geoff@reachthemarket.com>
2020-08-13 19:29:56 -0400

    Create Paint2.java

---
+++ Week7/Paint2.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+
+public class Paint2 {
+
+    public static void main(String[] args) {
+
+        Scanner scnr = new Scanner(System.in);
+        double wallHeight = 0.0;
+        double wallWidth = 0.0;
+        double wallArea = 0.0;
+        double gallonsPaintNeeded = 0.0;
+        double cansNeeded;
+
+        final double squareFeetPerGallons = 350.0;
+        final double gallonsPerCan = 1.0;
+
+        // Prompt user to input wall's height
+        System.out.println("Enter wall height (feet): ");
+        wallHeight = scnr.nextDouble();
+
+
+        // Prompt user to input wall's width
+        System.out.println("Enter wall width (feet): ");
+        wallWidth = scnr.nextDouble();
+
+        // Calculate and output wall area
+        wallArea = wallHeight * wallWidth;
+        System.out.println("Wall area:  " + wallArea + " square feet");
+
+        // Calculate and output the amount of paint in gallons needed to paint the wall
+        gallonsPaintNeeded = wallArea / squareFeetPerGallons;
+        System.out.println("Paint needed: " + gallonsPaintNeeded + " gallons");
+
+        // Calculate and output the number of paint cans needed to paint the wall,
+        // rounded up to nearest integer
+        // Complete this code block
+        // Math.ceil() Returns the smallest (closest to negative infinity) double value that is greater than or equal to
+        // the argument and is equal to a mathematical integer. https://docs.oracle.com/javase/8/docs/api/java/lang/Math.html#method.summary
+        cansNeeded = Math.ceil(gallonsPaintNeeded) / Math.ceil(gallonsPerCan);
+        System.out.println("Cans needed: " + cansNeeded + "can(s)");
+    }
+}
